cb545c2a1aea3c3351b81e6939aa2238d3fa57f2
Jeldo Arno Meppen <hi@jel.do>
2023-05-04 18:05:48 +0200

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Jeldo Arno Meppen on 04.05.23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
