3fb6483b4d58d54691b0f12190d67e5e1308e9c2
Jeldo Arno Meppen <hi@jel.do>
2023-05-05 00:51:56 +0200

    Add RecipeDetailView

---
+++ Cookcademy/Views/RecipeDetailView.swift
@@ -0,0 +1,60 @@
+//
+//  RecipeDetailView.swift
+//  Cookcademy
+//
+//  Created by Jeldo Arno Meppen on 04.05.23.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    let recipe: Recipe
+    
+    var body: some View {
+        VStack {
+            VStack {
+                HStack {
+                    Text("Author: \(recipe.mainInformation.author)")
+                        .padding()
+                        .font(.subheadline)
+                    Spacer()
+                }
+                HStack {
+                    Text(recipe.mainInformation.description)
+                        .padding()
+                        .font(.subheadline)
+                    Spacer()
+                }
+                List {
+                    Section(header: Text("Ingredients")) {
+                        ForEach(recipe.ingredients.indices, id: \.self) { index in
+                            let ingredient = recipe.ingredients[index]
+                            Text(ingredient.description)
+                        }
+                    }
+                    Section(header: Text("Steps")) {
+                        ForEach(recipe.directions.indices, id: \.self) { index in
+                            let direction = recipe.directions[index]
+                            HStack {
+                                Text("\(index + 1)")
+                                    .bold()
+                                Text("\(direction.isOptional ? "(Optional) " : "")"
+                                                    + "\(direction.description)")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .navigationTitle(recipe.mainInformation.name)
+    }
+}
+
+struct RecipeDetailView_Previews: PreviewProvider {
+    @State static var recipe = Recipe.testRecipes[0]
+    static var previews: some View {
+        NavigationView {
+            RecipeDetailView(recipe: recipe)            
+        }
+    }
+}
