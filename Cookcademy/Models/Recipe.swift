2b2157b549e3876efe1d5228fd78b081d13afea1
Jeldo Arno Meppen <hi@jel.do>
2023-05-04 19:04:08 +0200

    Add Unit type and description computed var

--- Cookcademy/Models/Recipe.swift
+++ Cookcademy/Models/Recipe.swift
@@ -30,7 +30,22 @@ struct MainInformation {
 struct Ingredient {
     var name: String
     var quantity: Double
-    var unit: String
+    var unit: Unit
+    
+    var description: String {
+        let formattedQuantity = String(format: "%g", quantity)
+        switch unit {
+        case .none:
+            let formattedName = quantity == 1 ? name : "\(name)s"
+            return "\(formattedQuantity) \(formattedName)"
+        default:
+            if quantity == 1 {
+                return "1 \(unit.singularName) \(name)"
+            } else {
+                return "\(formattedQuantity) \(unit.rawValue) \(name)"
+            }
+        }
+    }
     
     enum Unit: String, CaseIterable {
         case oz = "Ounces"
@@ -39,6 +54,10 @@ struct Ingredient {
         case tbs = "Tablespoons"
         case tsp = "Teaspoons"
         case none = "No units"
+        
+        var singularName: String {
+            return String(rawValue.dropLast())
+        }
     }
 }
 
